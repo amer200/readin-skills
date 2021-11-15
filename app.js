40a834434a634baacac778c4dc17259dda2e5f2f
amer <amermostaafa@gmail.com>
2021-11-16 00:45:45 +0200

    commit

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
 const ejs = require('ejs');
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
 // config body parser
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
 // parse application/json
 app.use(bodyParser.json())
 
@@ -25,11 +27,11 @@ app.use('/', mainRoutes);
 app.listen(port, () => {
     console.log(`Elearning app listening at http://localhost:${port}`)
     mongoose.connect('mongodb+srv://drhasan:753698@cluster0.utxxi.mongodb.net/speedreading')
-        .then( result => {
+        .then(result => {
             console.log('conectet to database')
         })
         .catch(err => {
             console.log(err);
         })
-  })
-  // RU}R2m=r];b! key
+})
+// RU}R2m=r];b! key
