83e3ecbb6deb827e0028cdf8247e0f3484d75500
amer <amermostaafa@gmail.com>
2021-11-19 20:03:33 +0200

    commit

--- routes/admin.js
+++ routes/admin.js
@@ -13,7 +13,7 @@ router.post('/delete-prag/:pId', auth,  adminControllers.DeleteParagraph);
 router.get('/edit-prag/:pId', auth,  adminControllers.EditParagraph);
 router.post('/edit-paragraph/:pId', auth,  adminControllers.postEditParagraph);
 // mix 
-router.get('/mix', auth,  adminControllers.getMix);
+//router.get('/mix', auth,  adminControllers.getMix);
 router.post('/add-mix', auth,  adminControllers.postAddMix);
 router.get('/edit-post/:postId', auth,  adminControllers.getEditPost);
 router.post('/edit-post/:postId', auth,  adminControllers.postEditPost);
