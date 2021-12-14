2660eb8fd9b1b3d24e15befdc729ca6cf06f4a03
amer <amermostaafa@gmail.com>
2021-12-14 20:42:55 +0200

    commit

--- app.js
+++ app.js
@@ -19,7 +19,8 @@ app.use(session({
     resave: false,
     saveUninitialized: true,
     cookie: {
-        maxAge: 1000 * 60 * 60 * 24
+        maxAge: 1000 * 60 * 60 * 24,
+        httpOnly: false
     }
 
 }));
